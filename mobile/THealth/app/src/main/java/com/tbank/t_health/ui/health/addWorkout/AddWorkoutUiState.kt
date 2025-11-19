464e5eaff4457ddd06e0cd703c42e46be5c22a36
DiCaprioLeonardo <149061169+LeonardoDaVinciCreator@users.noreply.github.com>
2025-11-19 20:40:26 +0500

    edited AddWorkoutUiState for mvvm and dependency injection

--- mobile/THealth/app/src/main/java/com/tbank/t_health/ui/health/addWorkout/AddWorkoutUiState.kt
+++ mobile/THealth/app/src/main/java/com/tbank/t_health/ui/health/addWorkout/AddWorkoutUiState.kt
@@ -1,3 +1,9 @@
 package com.tbank.t_health.ui.health.addWorkout
 
-data class AddWorkoutUiState()
+data class AddWorkoutUiState(
+    val name: String = "",
+    val type: String = "",
+    val durationMinutes: String = "",
+    val calories: String = "",
+    val date: String = ""
+)
